e20e783fbaa52ec0ef57beed35f6431dea9a1491
Matias Cutufia <matias.cutufia@hotmail.com>
2014-11-01 12:45:46 -0300

    Form Reservas

--- src/FrbaHotel/Generar Modificar Reserva/GenerarReserva.Designer.cs
+++ src/FrbaHotel/Generar Modificar Reserva/GenerarReserva.Designer.cs
@@ -74,7 +74,7 @@
             // 
             this.numericUpDown1.Location = new System.Drawing.Point(390, 75);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(41, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(121, 20);
             this.numericUpDown1.TabIndex = 4;
             // 
             // hotel
